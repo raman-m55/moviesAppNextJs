68cf14fdf0d9760f08070a3f8ea1a8a155e4f88a
raman-m55 <150461460+raman-m55@users.noreply.github.com>
2024-08-29 07:07:24 +0330

    Update fetchData.js

--- fetchData.js
+++ fetchData.js
@@ -2,8 +2,7 @@ const options = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZmZmMWQzNzI1OTY5MTYxYmY5ZDFiNTY1ZGQ3ODFlOSIsInN1YiI6IjY1NmUzY2JlM2RjMzEzMDBhY2FiZDQ0MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.X1EEOiHGjVTiz8puQ6c38bBA2u27UHGfIeDS-zatF6g'
-    } ,
+      Authorization: process.env.KEY
     cache: 'no-store'
   };
 
