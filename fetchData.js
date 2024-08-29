9a7aa0184dca46a98e84a825747a40d04d45720e
raman-m55 <150461460+raman-m55@users.noreply.github.com>
2024-08-29 07:17:20 +0330

    Update fetchData.js

--- fetchData.js
+++ fetchData.js
@@ -2,12 +2,12 @@ const options = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: process.env.KEY,
-  };
-
+      Authorization: process.env.KEY
+    } 
+};
 
 export const fetchData = async (endpoint) => {
-        const response = await fetch(`https://api.themoviedb.org/3/${endpoint}` ,  options);
-        const data = await response.json();
-        return data;
+    const response = await fetch(`https://api.themoviedb.org/3/${endpoint}`, options);
+    const data = await response.json();
+    return data;
 }
